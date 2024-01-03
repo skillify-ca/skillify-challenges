82e6eb50c077b75ec4b36c0342b7a32820c5716e
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2024-01-03 13:05:19 -0500

    Update delay.ts

--- src/promises/delay.ts
+++ src/promises/delay.ts
@@ -1,5 +1,7 @@
 // delay a promise
 
-export function delay(ms: number): Promise<any> {}
+export function delay(ms: number): Promise<any> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 delay(5000).then(() => console.log("1 second passed"));
